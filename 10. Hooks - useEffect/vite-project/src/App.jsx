b7443e0f464edb219dd7fae8e2ec1fae4db17c83
Saumya Som <saumyasom92@gmail.com>
2025-09-10 15:05:48 +0530

    React - useEffect

--- 10. Hooks - useEffect/vite-project/src/App.jsx
+++ 10. Hooks - useEffect/vite-project/src/App.jsx
@@ -17,49 +17,74 @@ function App() {
 
 
   /**
-   * When First Time Open The URl The App Component Will Render. When It Will Render
-   * <Navbar color={color}/> Component Render..Because It Written in First in Main Div, When this Navbar Component Render 
+   * React always renders parent component first (App in this case). Rendering = React building the "virtual DOM tree" of what UI should look like.
    * 
-   * First Alert Message "Hey I Will Run on Every Render : Navbar"
+   * During this phase, React calls App() to know what to render inside it. When React reaches <Navbar color={color}/> inside App’s JSX, it will then 
+   * render the Navbar component.
    * 
-   * Second Alert Message "Hey I Will Run on First Render : Navbar"
+   * So the render order is:
+   * 1. App runs first.
+   * 2. Then, inside App's JSX, Navbar runs.
    * 
-   * Third Alert Message "Hey I Will Run on Dependancy Changed : Navbar"... It Comes Because Intially Color State 
-   * set None to 0, So Dependancy was Changed
+   * Important: useEffect does not run during rendering. It runs after the DOM is painted (commit phase).
    * 
-   * Navbar Rendering Completed
    * 
-   * Now Next Div Will Render  Fourth Alert Message "Hey I Will Run on Dependancy Changed : Main App".. Again Count
-   * State set None to 0, So Dependancy was Changed. useEffect() Will Run.. When It Run setColor(count + 1) This is also 
-   * run... Now color set to 0 + 1 => 1 .. And It Will Pass as Props to <Navbar color={color}/>
+   * So the lifecycle looks like this:
    * 
-   * Now Props is Changed So <Navbar color={color}/> Component Again Re-Render
+   * 1.App renders → builds JSX tree.
+   *       While rendering, React sees <Navbar color={color}/>.
+   *       It calls Navbar() to know what to render inside it.
    * 
-   * Fifth Alert Message "Hey I Will Run on Every Render : Navbar"
+   * 2. Navbar renders → React builds Navbar’s JSX tree
+   * 3. React commits both App and Navbar UI to the DOM.
    * 
-   * Though color was chaged 0 to 1 , Mean Dependancy was Changed... Case 3 UseEffect Run
+   * 4. Now, React runs all useEffect hooks:
+   *        First useEffect in Navbar (since it mounted).
+   *        Then useEffect in App.
    * 
-   * Sixth Alert Message "Hey I Will Run on Dependancy Changed : Navbar"
+   * First render order:
+   *  App → then Navbar (because Navbar is inside App).
    * 
-
-   * When Firs Time Click is Count ... In The Back.. State Was Changed. When State
-   * was changed the Whole App Component Will Re-Render. When Whole Componentr
-   * re-rendered <Navbar color={color}/> Component Will Re-Render.
+   * First effect order:
+   * Navbar useEffect runs before App useEffect, because React runs child effects first, then parent effects.
    * 
-   * So It First Alert Message "Hey I Will Run on Every Render : Navbar"
+   * Render + Effect Flow : 
+   * App renders (initial count=0, color=0)
+   * Inside App’s JSX → Navbar renders (color=0)
    * 
-   * State Was Changed.. So in App Component call useEffect() before Dependancy
-   * was changed.. Second Alert Message "Hey I Will Run on Dependancy Changed : Main App"
+   * During render, no effects run yet only commit happens. 
    * 
-   * In App Component useEffect() we serColor(count + 1),  So Color State was changed, This 
-   * color passed as Props to <Navbar color={color}/> Component.. When Props Was Changed
-   * <Navbar color={color}/> Component Will Re-Render.
+   * Now, React runs all effects (child before parent).
+   * Effects order:
    * 
-   * When It Again Re-Render Third Alert Message "Hey I Will Run on Every Render : Navbar"
+   * Navbar Case 1 → "Hey I Will Run on Every Render : Navbar"
+   * Navbar Case 2 → "Hey I Will Run on First Render : Navbar"
+   * Navbar Case 3 (dependency: color=0 changed from undefined) -> "Hey I Will Run on Dependancy Changed : Navbar"
    * 
-   * Next, in Navbar Component useEffect() color was changed From. So Dependancy was changed.
+   * App useEffect [count] (dependency: count=0 changed from undefined) -> "Hey I Will Run on Dependancy Changed : Main App"
+   * Inside it: setColor(count+1) → updates color to 1.
+   * State update (color). So It triggers for re-render.
    * 
-   * Fourth Alert Message "Hey I Will Run on Dependancy Changed : Navbar"
+   * App renders again (count=0, color=1).
+   * Navbar renders again with color=1.
+   *
+   * Child Component Effect Run First
+   * Navbar Case 1 → "Hey I Will Run on Every Render : Navbar"
+   * Navbar Case 3 (dependency: color changed from 0 → 1) → "Hey I Will Run on Dependancy Changed : Navbar"
+   * App useEffect does not run (since count didn’t change this time).
+   * 
+   * 
+   * When Click The Button :
+   * count goes from 0 → 1. State Was Updated. App re-renders (count=1, still color=1 for now).Navbar renders with color=1. Though Its Re Rendering, App Render First.. Navbar Render Secound
+   * But Child UseEffect Run First
+   * 
+   * Navbar Case 1 → "Hey I Will Run on Every Render : Navbar"
+   * App useEffect [count] (dependency count changed 0→1) → "Hey I Will Run on Dependancy Changed : Main App"
+   * Inside it: setColor(count+1) → updates color=2.
+   * 
+   * App renders again (count=1, color=2). Navbar renders again with color=2. Navbar Useeffect Run First
+   * Navbar Case 1 → "Hey I Will Run on Every Render : Navbar"
+   * Navbar Case 3 (dependency: color changed 1→2) → "Hey I Will Run on Dependancy Changed : Navbar"
    * 
    */
 
